5d910492ac312b9cf8461fac556097c5921e615e
Yukinari <c4he@ucsd.edu>
2022-11-14 02:42:24 -0700

    Modify scripts and remove uncessary files

---
+++ mtrace_analysis/memcached/run-ycsb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# dependencies
+# dependencies
+sudo apt update
+sudo apt install -y default-jre maven
+
+# clone YCSB
+cd $WORKDIR
+git clone https://github.com/brianfrankcooper/YCSB.git
+
+cd YCSB
+
+mvn -pl site.ycsb:memcached-binding -am clean package
+
+./bin/ycsb.sh load memcached -s -P workloads/workloada \
+    -p "memcached.hosts=127.0.0.1"
+
+./bin/ycsb.sh run memcached -s -P workloads/workloada \
+    -p "memcached.hosts=127.0.0.1"
--- mtrace_analysis/memcached/run.sh
+++ mtrace_analysis/memcached/run.sh
@@ -3,16 +3,16 @@
 WORKDIR=$( pwd; )
 MEMCACHED_VERSION=1.6.17
 
+# dependencies
+sudo apt update
+sudo apt install -y libevent-dev
+
 # compile memcached from source
 wget https://memcached.org/files/memcached-${MEMCACHED_VERSION}.tar.gz
 tar -zxvf memcached-${MEMCACHED_VERSION}.tar.gz
 rm memcached-${MEMCACHED_VERSION}.tar.gz
 cd memcached-${MEMCACHED_VERSION}
 ./configure && make
-
-# clone YCSB
-cd $WORKDIR
-git clone https://github.com/brianfrankcooper/YCSB.git
 
 cd $WORKDIR/../../tools/intel_pin/source/tools/SimpleExamples
 make all TARGET=intel64
