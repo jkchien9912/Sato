c3a33f1cfa3815d98645eb3d025e65605a8351d2
Yukinari <c4he@ucsd.edu>
2022-11-17 19:33:10 -0700

    update scripts

--- mtrace_analysis/memcached/run-memcached.sh
+++ mtrace_analysis/memcached/run-memcached.sh
@@ -14,6 +14,8 @@ rm memcached-${MEMCACHED_VERSION}.tar.gz
 cd memcached-${MEMCACHED_VERSION}
 ./configure && make
 
-cd $WORKDIR/../../tools/intel-pin/source/tools/SimpleExamples
+cd $WORKDIR/../../tools
+tar -xvzf intel-pin.tar.gz
+cd pin-3.25-98650-g8f6168173-gcc-linux/source/tools/SimpleExamples
 make all TARGET=intel64
 ../../../pin -t obj-intel64/pinatrace.so -- ${WORKDIR}/memcached-${MEMCACHED_VERSION}/memcached
