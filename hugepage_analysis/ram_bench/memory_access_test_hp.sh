829bac244327c535c4f29335b7374fedcb44d3a2
heidic <heidic@node-0.heidic-141792.ucsdcse291hfa22-pg0.utah.cloudlab.us>
2022-12-03 23:47:18 -0700

    Modify benchmark scripts

--- hugepage_analysis/ram_bench/memory_access_test_hp.sh
+++ hugepage_analysis/ram_bench/memory_access_test_hp.sh
@@ -3,13 +3,13 @@
 g++ list_one_traversal.cpp -o list_one_traversal
 
 # Sequential access using huge page 
-for i in {6..30}
+for i in {6..31}
 do
-    sudo perf stat --output ./seq_hp_pf.txt --append env LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./list_one_traversal sequential $i >> ./seq_hp_output.txt
+    sudo perf stat -e 'faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references' --output ./seq_hp_pf.txt --append env LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./list_one_traversal sequential $i >> ./seq_hp_output.txt
 done
 
 # Random access using huge page 
-for i in {6..30}
+for i in {6..31}
 do
-    sudo perf stat --output ./random_hp_pf.txt --append env LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./list_one_traversal sequential $i >> ./random_hp_output.txt
+    sudo perf stat -e 'faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references' --output ./random_hp_pf.txt --append env LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./list_one_traversal sequential $i >> ./random_hp_output.txt
 done
--- hugepage_analysis/ram_bench/memory_access_test_sp.sh
+++ hugepage_analysis/ram_bench/memory_access_test_sp.sh
@@ -2,14 +2,19 @@
 
 g++ list_one_traversal.cpp -o list_one_traversal
 
+rm -f ./seq_sp_output.txt
+rm -f ./seq_sp_pf.txt
+rm -f ./random_sp_output.txt
+rm -f ./random_sp_pf.txt
+
 # Sequential access using standard page 
-for i in {6..30}
+for i in {6..31}
 do
-    sudo perf stat --output ./seq_sp_pf.txt --append ./list_one_traversal sequential $i >> ./seq_sp_output.txt
+    sudo perf stat -e 'faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references' --output ./seq_sp_pf.txt --append ./list_one_traversal sequential $i >> ./seq_sp_output.txt
 done
 
 # Random access using standard page
-for i in {6..30}
+for i in {6..31}
 do 
-    sudo perf stat --output ./random_sp_pf.txt  --append ./list_one_traversal random $i >> ./random_sp_output.txt
+    sudo perf stat -e 'faults,dTLB-loads,dTLB-load-misses,cache-misses,cache-references' --output ./random_sp_pf.txt  --append ./list_one_traversal random $i >> ./random_sp_output.txt
 done 
